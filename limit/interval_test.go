7f407b519f9d0910ca3ae7a4735964ec17c09a0f
tomonoharuki <tomono@wacul.co.jp>
2019-10-25 19:08:17 +0900

    test: add multi key test

--- limit/interval_test.go
+++ limit/interval_test.go
@@ -6,6 +6,8 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 type intervalTest struct {
@@ -85,16 +87,41 @@ func TestIntervalFactory(t *testing.T) {
 	}
 }
 
+const groupKey = "x-key"
+
+func groupKeyByHeader(r *http.Request) string {
+	return r.Header.Get(groupKey)
+}
+
+type intervalMultiTest struct {
+	lastRequested map[string]time.Time
+	intervals     map[string][]time.Duration
+	l             sync.Mutex
+}
+
+func (it *intervalMultiTest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	rt := time.Now()
+
+	it.l.Lock()
+	key := r.Header.Get(groupKey)
+	it.intervals[key] = append(it.intervals[key], rt.Sub(it.lastRequested[key]))
+	it.lastRequested[key] = rt
+	it.l.Unlock()
+}
+
 func TestIntervalWithExpire(t *testing.T) {
-	it := &intervalTest{}
+	it := &intervalMultiTest{
+		lastRequested: make(map[string]time.Time),
+		intervals:     make(map[string][]time.Duration),
+	}
 	s := httptest.NewServer(it)
 	defer s.Close()
 
 	exInterval := 100 * time.Millisecond
 
 	transport := NewIntervalTransport(exInterval)
-	transport.GroupKeyFunc = GroupKeyByHost
-	expire := 10 * time.Millisecond
+	transport.GroupKeyFunc = groupKeyByHeader
+	expire := 300 * time.Millisecond
 	transport.ExpireCheckInterval = &expire
 	defer transport.Close()
 
@@ -102,34 +129,56 @@ func TestIntervalWithExpire(t *testing.T) {
 		Transport: transport,
 	}
 
-	wg := sync.WaitGroup{}
-	numReq := 10
-	wg.Add(numReq)
-	for i := 0; i < numReq; i++ {
-		time.Sleep(20 * time.Millisecond)
+	request := func(wg *sync.WaitGroup, key string) {
+		req, err := http.NewRequest("GET", s.URL, nil)
+		require.NoError(t, err)
+		req.Header.Set(groupKey, key)
 		go func() {
-			testClient.Get(s.URL)
+			testClient.Do(req)
 			wg.Done()
 		}()
 	}
-	wg.Wait()
 
-	if it.minInterval < exInterval-(10*time.Millisecond) { // handler may delay
-		t.Errorf("min request interval to server must grater than %s actual %s", exInterval.String(), it.minInterval.String())
-	}
+	numReq := 10
+	keys := []string{"a", "b", "c"}
 
-	time.Sleep(20 * time.Millisecond)
-
+	// first
 	{
 		wg := sync.WaitGroup{}
-		numReq := 10
-		wg.Add(numReq)
 		for i := 0; i < numReq; i++ {
-			go func() {
-				testClient.Get(s.URL)
-				wg.Done()
-			}()
+			wg.Add(len(keys))
+			for _, key := range keys {
+				request(&wg, key)
+			}
 		}
 		wg.Wait()
+	}
+
+	// waiting expire
+	time.Sleep(300 * time.Millisecond)
+
+	// second(create new channel after expire)
+	{
+		wg := sync.WaitGroup{}
+		for i := 0; i < numReq; i++ {
+			wg.Add(len(keys))
+			for _, key := range keys {
+				request(&wg, key)
+			}
+		}
+		wg.Wait()
+	}
+
+	// assert all request intervals
+	assertInterval := func(key string) {
+		limit := exInterval - (10 * time.Millisecond) // handler may delay
+		for _, interval := range it.intervals[key] {
+			if interval < limit {
+				t.Errorf("min request interval to server must grater than %s actual %s", exInterval.String(), interval.String())
+			}
+		}
+	}
+	for _, key := range keys {
+		assertInterval(key)
 	}
 }
